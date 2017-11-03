37f1b5ccf676ca413d647cc2be541b4c3628fbff
Rena Wang <31461970+Rena07@users.noreply.github.com>
2017-11-03 18:15:18 -0500

    Add files via upload

--- tableCommands.py
+++ tableCommands.py
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 #!/usr/bin/python
 
 import sqlite3
@@ -51,57 +50,3 @@ def updateEmail(user, userEmail):
 	act.execute("UPDATE AccountInfo SET EMAIL = userEmail WHERE USERNAME = user")
 	dbase.commit()
 	dbase.close()
-=======
-#!/usr/bin/python
-
-import sqlite3
-
-def insertAccount(first, last, user, userPassword, userEmail, userPhoneNumber):
-	dbase = sqlite3.connect('Accounts.db')
-	act = dbase.cursor()
-	row = (first, last, user, userPassword, userEmail, userPhoneNumber)
-	act.execute("INSERT INTO Accounts (FIRSTNAME, LASTNAME, USERNAME, PASSWORD, EMAIL, PHONENUMBER) VALUES (?, ?, ?, ?, ?, ?)", row)
-	dbase.commit()
-	dbase.close()
-
-#insertAccount('Brian', 'Wang', 'brianwang', 'cs196', 'brian@gmail.com', 'asdf')
-
-
-def retrieveUser(user):
- 	dbase = sqlite3.connect('Accounts.db')
- 	act = dbase.cursor()
- 	str = user
- 	act.execute("SELECT ID, FIRSTNAME, LASTNAME, USERNAME, PASSWORD, EMAIL, PHONENUMBER FROM Accounts WHERE USERNAME is (?, ? ,?,?,?,?,?,?,?)", user)
- 	dbase.commit()
- 	dbase.close()
-
-print(retrieveUser('brianwang'))
-
-def retrieveUser(userPhoneNumber):
-	dbase = sqlite3.connect('Accounts.db')
-	act = dbase.cursor()
-	act.execute("SELECT ID, FIRSTNAME, LAST, USERNAME, PASSWORD, EMAIL, PHONENUMBER FROM AccountInfo WHERE PHONENUMBER = userPhoneNumber")
-	dbase.commit()
-	dbase.close()
-
-def updatePhone(user, userPhoneNumber):
-	dbase = sqlite3.connect('Accounts.db')
-	act = dbase.cursor()
-	act.execute("UPDATE AccountInfo SET PHONENUMBER = userPhoneNumber WHERE USERNAME = user")
-	dbase.commit()
-	dbase.close()
-
-def updateName(user, userFirst, userLast):
-	dbase = sqlite3.connect('Accounts.db')
-	act = dbase.cursor()
-	act.execute("UPDATE AccountInfo SET FIRSTNAME = userFirst, LAST = userLast WHERE USERNAME = user")
-	dbase.commit()
-	dbase.close()
-
-def updateEmail(user, userEmail):
-	dbase = sqlite3.connect('Accounts.db')
-	act = dbase.cursor()
-	act.execute("UPDATE AccountInfo SET EMAIL = userEmail WHERE USERNAME = user")
-	dbase.commit()
-	dbase.close()
->>>>>>> 792eec7c9dc046c18f0610fb87645a75fd2bd414
